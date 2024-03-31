78ce4fd190d977bd155ecaa5df7e0421827a7021
Nov1c444 <857526207@qq.com>
2024-03-31 18:52:43 +0800

    fix lint issue😁

--- app/tags/page.tsx
+++ app/tags/page.tsx
@@ -8,7 +8,7 @@ export const metadata = genPageMetadata({ title: 'Tags', description: 'Things I 
 
 export default async function Page() {
   const tagCounts = tagData as Record<string, number>
-  
+
   const tagKeys = Object.keys(tagCounts)
   const sortedTags = tagKeys.sort((a, b) => tagCounts[b] - tagCounts[a])
   return (
