3c7a818ed4e4ab819106bf2f4276428fa628680c
Nov1c444 <857526207@qq.com>
2024-03-31 18:47:29 +0800

    import aigc weekly

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -17,7 +17,6 @@ const LayoutWrapper = ({ children }: Props) => {
     <SectionContainer>
       <div className={`${inter.className} flex h-screen flex-col justify-between font-sans`}>
         <Header />
-        <main className="mb-auto">{children}</main>
         <Footer />
       </div>
     </SectionContainer>
